fa3b01417bafe315878141d23807e6b09c05b1fb
Your Name <you@example.com>
2023-03-25 11:53:18 +0545

    without the link correctly shown in mobile menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,154 +1,97 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <div>
-      <nav className="bg-gray-200">
-        <div className="container mx-auto px-6 py-4 flex justify-between items-center">
-          <div>
-            <a
-              className="bg-red-400 text-gray-100 hover:text-gray-50 hover:bg-gray-700 px-3 py-2 rounded-md text-xl font-bold shadow-md"
-              href='/intro'
-            >
-              Bhairab IT Solution
-            </a>
-          </div>
-          <div className="flex items-center">
-            <div className="hidden md:block">
-              <div className="flex gap-4">
-                
-
-                <Link
-                  to="/intro"
-                  className="bg-gray-800 text-gray-100 hover:text-gray-50 hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium shadow-md flex"
-                >
-                  Home
-                </Link>
-
-                <Link
-                  to="/services"
-                  className="bg-gray-800 text-gray-100 hover:text-gray-50 hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium shadow-md flex"
-                >
-                  Services
-                </Link>
-
-                <Link
-                to="/aboutus"
-                className="bg-gray-800 text-gray-100 hover:text-gray-50 hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium shadow-md flex" >About us</Link>
-                
-
-                <Link
-                  to="/team"
-                  className="bg-gray-800 text-gray-100 hover:text-gray-50 hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium shadow-md flex"
-                >
-                  Contact us
-                </Link>
-
-                <Link
-                to='/portfolio'
-                className="bg-gray-800 text-gray-100 hover:text-gray-50 hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium shadow-md flex" >Work Portfolio</Link>
-
-                <Link
-                to='/socialmediaservice'
-                className="bg-gray-800 text-gray-100 hover:text-gray-50 hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium shadow-md flex" >Social Media Service
-                </Link>
+    <nav className="flex items-center justify-between flex-wrap bg-gray-800 p-6">
+      <div className="flex items-center flex-shrink-0 text-white mr-6">
+        <span className="font-semibold text-xl tracking-tight ml-2 hidden sm:block">
+          Bhairab IT Solutions
+        </span>
+      </div>
+      <div className="block sm:hidden">
+        <button
+          onClick={toggleMenu}
+          className="flex items-center px-3 py-2 border rounded text-gray-400 border-gray-500 hover:text-white hover:border-white"
+        >
+          <svg
+            className="fill-current h-3 w-3"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <title>Menu</title>
+            {isMenuOpen ? (
+              <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M17 14H3V10H17V14ZM17 8H3V4H17V8ZM3 15H17V17H3V15Z"
+                fill="white"
+              />
+            ) : (
+              <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M0 3H20V5H0V3ZM0 9H20V11H0V9ZM0 15H20V17H0V15Z"
+                fill="white"
+              />
+            )}
+          </svg>
+        </button>
+      </div>
+      <div
+        className={`${isMenuOpen ? "block" : "hidden"
+          } w-full block flex-grow sm:flex sm:items-center sm:w-auto`}
+      >
+        <div className="flex text-sm sm:flex-grow">
 
 
-              </div>
-            </div>
-            <div className="md:hidden">
-              <button
-                type="button"
-                className="bg-gray-800 text-gray-100 hover:text-gray-50 hover:bg-gray-700 px-2 py-1 rounded-md text-sm font-medium shadow-md"
-                aria-controls="mobile-menu"
-                aria-expanded="false"
-              >
-                <span className="sr-only">Open main menu</span>
-                <svg
-                  className="block h-6 w-6"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M4 6h16M4 12h16M4 18h16"
-                  />
-                </svg>
-                <svg
-                  className="hidden h-6 w-6"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                </svg>
-              </button>
-            </div>
-          </div>
+          <Link
+            to="/intro"
+            className="bg-gray-800 text-gray-100 hover:text-gray-50 hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium shadow-md flex"
+          >
+            Home
+          </Link>
+
+          <Link
+            to="/services"
+            className="bg-gray-800 text-gray-100 hover:text-gray-50 hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium shadow-md flex"
+          >
+            Services
+          </Link>
+
+          <Link
+            to="/aboutus"
+            className="bg-gray-800 text-gray-100 hover:text-gray-50 hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium shadow-md flex" >About us</Link>
+
+
+          <Link
+            to="/team"
+            className="bg-gray-800 text-gray-100 hover:text-gray-50 hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium shadow-md flex"
+          >
+            Contact us
+          </Link>
+
+          <Link
+            to='/portfolio'
+            className="bg-gray-800 text-gray-100 hover:text-gray-50 hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium shadow-md flex" >Work Portfolio</Link>
+
+          <Link
+            to='/socialmediaservice'
+            className="bg-gray-800 text-gray-100 hover:text-gray-50 hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium shadow-md flex" >Social Media Service
+          </Link>
+
+
+
         </div>
-        <div className="md:hidden" id="mobile-menu">
-          <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-            {/* <a
-              className="bg-gray-800 text-gray-100 hover:text-gray-50 hover:bg-gray-700 block px-3 py-2 rounded-md text-base font-medium"
-              href="#"
-            >
-              Home
-            </a> */}
+      </div>
+    </nav>
+  );
+};
 
-            <Link
-                  to="/intro"
-                  className="bg-gray-800 text-gray-100 hover:text-gray-50 hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium shadow-md flex"
-                >
-                  Home
-                </Link>
-
-                <Link
-                  to="/services"
-                  className="bg-gray-800 text-gray-100 hover:text-gray-50 hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium shadow-md flex"
-                >
-                  Services
-                </Link>
-
-                <Link
-                to="/aboutus"
-                className="bg-gray-800 text-gray-100 hover:text-gray-50 hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium shadow-md flex" >About us</Link>
-                
-
-                <Link
-                  to="/team"
-                  className="bg-gray-800 text-gray-100 hover:text-gray-50 hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium shadow-md flex"
-                >
-                  Contact us
-                </Link>
-
-                <Link
-                to='/portfolio'
-                className="bg-gray-800 text-gray-100 hover:text-gray-50 hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium shadow-md flex" >Work Portfolio</Link>
-
-                <Link
-                to='/socialmediaservice'
-                className="bg-gray-800 text-gray-100 hover:text-gray-50 hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium shadow-md flex" >Social Media Service
-                </Link>
-            
-          </div>
-        </div>
-      </nav>
-
-    </div>
-  )
-}
-
-export default Header
+export default Header;
