009472f9ed9e6f3bbb1ae0fbc97d798973e9251a
Your Name <you@example.com>
2023-03-27 17:47:10 +0545

    added icon in header item for both mobile and desktop

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,11 +11,11 @@ const Header = () => {
   return (
     <nav className="flex items-center justify-between flex-wrap bg-gray-800 p-6 ">
       <div class="flex items-center flex-shrink-0 text-white mr-48">
-    <a href="/">
-      <img src="public\assets\bhairabictlogo.png" alt="Bhairab IT Solutions logo" width="100" height="50"
-        class="transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110" />
-    </a>
-  </div>
+        <a href="/">
+          <img src="public\assets\bhairabictlogo.png" alt="Bhairab IT Solutions logo" width="100" height="50"
+            class="transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110" />
+        </a>
+      </div>
 
       <div className="block sm:hidden">
         <button
@@ -53,97 +53,197 @@ const Header = () => {
         <div className="flex text-sm sm:flex-grow">
 
           <div className="sm:hidden">
-            <Link
+          <Link
               to="/intro"
-              className="bg-gray-800 text-gray-100 hover:text-gray-50 hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium shadow-md flex"
+              className="bg-gray-800 text-gray-100 hover:text-gray-50 hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium shadow-md flex flex-col items-center"
             >
               Home
+              <br />
+              <div class="flex flex-row justify-center items-center gap-2">
+                <div>
+                  <img src="https://img.icons8.com/color/256/home-page.png" alt="Home" class="w-6 h-6 my-2" />
+                </div>
+              </div>
             </Link>
 
             <Link
               to="/services"
-              className="bg-gray-800 text-gray-100 hover:text-gray-50 hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium shadow-md flex"
+              className="bg-gray-800 text-gray-100 hover:text-gray-50 hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium shadow-md flex flex-col items-center"
             >
               Services
+              <br />
+              <div class="flex flex-row justify-center items-center gap-2">
+                <div>
+                  <img src="https://img.icons8.com/plasticine/256/services.png" alt="Services" class="w-6 h-6 my-2" />
+                </div>
+              </div>
             </Link>
 
             <Link
               to="/aboutus"
-              className="bg-gray-800 text-gray-100 hover:text-gray-50 hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium shadow-md flex" >About us</Link>
-
+              className="bg-gray-800 text-gray-100 hover:text-gray-50 hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium shadow-md flex flex-col items-center"
+            >
+              About us
+              <br />
+              <div class="flex flex-row justify-center items-center gap-2">
+                <div>
+                  <img src="https://img.icons8.com/external-tanah-basah-glyph-tanah-basah/256/external-about-social-media-ui-tanah-basah-glyph-tanah-basah.png" alt="About Us" class="w-6 h-6 my-2" />
+                </div>
+              </div>
+            </Link>
 
             <Link
               to="/team"
-              className="bg-gray-800 text-gray-100 hover:text-gray-50 hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium shadow-md flex"
+              className="bg-gray-800 text-gray-100 hover:text-gray-50 hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium shadow-md flex flex-col items-center"
             >
               Contact us
+              <br />
+              <div class="flex flex-row justify-center items-center gap-2">
+                <div>
+                  <img src="https://img.icons8.com/external-flatart-icons-lineal-color-flatarticons/256/external-contact-us-contact-us-flatart-icons-lineal-color-flatarticons.png" alt="Contact Us" class="w-6 h-6 my-2" />
+                </div>
+              </div>
             </Link>
 
             <Link
               to='/portfolio'
-              className="bg-gray-800 text-gray-100 hover:text-gray-50 hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium shadow-md flex" >Work Portfolio</Link>
+              className="bg-gray-800 text-gray-100 hover:text-gray-50 hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium shadow-md flex flex-col items-center"
+            >
+              Work Portfolio
+              <br />
+              <div class="flex flex-row justify-center items-center gap-2">
+                <div>
+                  <img src="https://img.icons8.com/fluency/256/portfolio.png" alt="Work Portfolio" class="w-6 h-6 my-2" />
+                </div>
+              </div>
+            </Link>
+
+
 
             <Link
               to='/socialmediaservice'
-              className="bg-gray-800 text-gray-100 hover:text-gray-50 hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium shadow-md flex" >Social Media Service
+              className="bg-gray-800 text-gray-100 hover:text-gray-50 hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium shadow-md flex flex-col items-center"
+            >
+              Social Media Service
+              <br />
+              <div class="flex flex-row justify-center items-center gap-2">
+                <div>
+                  <img src="https://cdn.icon-icons.com/icons2/1584/PNG/512/3721672-instagram_108066.png" alt="Instagram" class="w-6 h-6 my-2" />
+                </div>
+                <div>
+                  <img src="https://cdn.icon-icons.com/icons2/122/PNG/512/twitter_socialnetwork_20007.png" alt="Twitter" class="w-6 h-6 my-2" />
+                </div>
+                <div>
+                  <img src="https://cdn.icon-icons.com/icons2/805/PNG/512/linkedin_icon-icons.com_65929.png" alt="LinkedIn" class="w-6 h-6 my-2" />
+                </div>
+                <div>
+                  <img src="https://cdn.icon-icons.com/icons2/2972/PNG/512/tiktok_logo_icon_186896.png" alt="TikTok" class="w-6 h-6 my-2" />
+                </div>
+                <div>
+                  <img src="https://cdn.icon-icons.com/icons2/3658/PNG/512/meta_fb_communication_social_media_katana_facebook_icon_228415.png" alt="Facebook" class="w-6 h-6 my-2" />
+                </div>
+              </div>
             </Link>
+
+
+
           </div>
 
           {/* LAPTOP SCREEN  */}
           <div className="hidden sm:flex sm:flex-row">
+
             <Link
               to="/intro"
-              className="bg-gray-800 text-gray-100 hover:text-gray-50 hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium shadow-md flex"
+              className="bg-gray-800 text-gray-100 hover:text-gray-50 hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium shadow-md flex flex-col items-center"
             >
               Home
+              <br />
+              <div class="flex flex-row justify-center items-center gap-2">
+                <div>
+                  <img src="https://img.icons8.com/color/256/home-page.png" alt="Home" class="w-6 h-6 my-2" />
+                </div>
+              </div>
             </Link>
 
             <Link
               to="/services"
-              className="bg-gray-800 text-gray-100 hover:text-gray-50 hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium shadow-md flex"
+              className="bg-gray-800 text-gray-100 hover:text-gray-50 hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium shadow-md flex flex-col items-center"
             >
               Services
+              <br />
+              <div class="flex flex-row justify-center items-center gap-2">
+                <div>
+                  <img src="https://img.icons8.com/plasticine/256/services.png" alt="Services" class="w-6 h-6 my-2" />
+                </div>
+              </div>
             </Link>
 
             <Link
               to="/aboutus"
-              className="bg-gray-800 text-gray-100 hover:text-gray-50 hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium shadow-md flex" >About us</Link> 
-
+              className="bg-gray-800 text-gray-100 hover:text-gray-50 hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium shadow-md flex flex-col items-center"
+            >
+              About us
+              <br />
+              <div class="flex flex-row justify-center items-center gap-2">
+                <div>
+                  <img src="https://img.icons8.com/external-tanah-basah-glyph-tanah-basah/256/external-about-social-media-ui-tanah-basah-glyph-tanah-basah.png" alt="About Us" class="w-6 h-6 my-2" />
+                </div>
+              </div>
+            </Link>
 
             <Link
               to="/team"
-              className="bg-gray-800 text-gray-100 hover:text-gray-50 hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium shadow-md flex"
+              className="bg-gray-800 text-gray-100 hover:text-gray-50 hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium shadow-md flex flex-col items-center"
             >
               Contact us
+              <br />
+              <div class="flex flex-row justify-center items-center gap-2">
+                <div>
+                  <img src="https://img.icons8.com/external-flatart-icons-lineal-color-flatarticons/256/external-contact-us-contact-us-flatart-icons-lineal-color-flatarticons.png" alt="Contact Us" class="w-6 h-6 my-2" />
+                </div>
+              </div>
             </Link>
 
             <Link
               to='/portfolio'
-              className="bg-gray-800 text-gray-100 hover:text-gray-50 hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium shadow-md flex" >Work Portfolio</Link>
+              className="bg-gray-800 text-gray-100 hover:text-gray-50 hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium shadow-md flex flex-col items-center"
+            >
+              Work Portfolio
+              <br />
+              <div class="flex flex-row justify-center items-center gap-2">
+                <div>
+                  <img src="https://img.icons8.com/fluency/256/portfolio.png" alt="Work Portfolio" class="w-6 h-6 my-2" />
+                </div>
+              </div>
+            </Link>
+
+
+
             <Link
-  to='/socialmediaservice'
-  className="bg-gray-800 text-gray-100 hover:text-gray-50 hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium shadow-md flex flex-col items-center"
->
-  Social Media Service 
-  <br />
-  <div class="flex flex-row justify-center items-center gap-2">
-    <div>
-      <img src="https://cdn.icon-icons.com/icons2/1584/PNG/512/3721672-instagram_108066.png" alt="Instagram" class="w-6 h-6 my-2" />
-    </div>
-    <div>
-      <img src="https://cdn.icon-icons.com/icons2/122/PNG/512/twitter_socialnetwork_20007.png" alt="Twitter" class="w-6 h-6 my-2" />
-    </div>
-    <div>
-      <img src="https://cdn.icon-icons.com/icons2/805/PNG/512/linkedin_icon-icons.com_65929.png" alt="LinkedIn" class="w-6 h-6 my-2" />
-    </div>
-    <div>
-      <img src="https://cdn.icon-icons.com/icons2/2972/PNG/512/tiktok_logo_icon_186896.png" alt="TikTok" class="w-6 h-6 my-2" />
-    </div>
-    <div>
-      <img src="https://cdn.icon-icons.com/icons2/3658/PNG/512/meta_fb_communication_social_media_katana_facebook_icon_228415.png" alt="Facebook" class="w-6 h-6 my-2" />
-    </div>
-  </div>
-</Link>
+              to='/socialmediaservice'
+              className="bg-gray-800 text-gray-100 hover:text-gray-50 hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium shadow-md flex flex-col items-center"
+            >
+              Social Media Service
+              <br />
+              <div class="flex flex-row justify-center items-center gap-2">
+                <div>
+                  <img src="https://cdn.icon-icons.com/icons2/1584/PNG/512/3721672-instagram_108066.png" alt="Instagram" class="w-6 h-6 my-2" />
+                </div>
+                <div>
+                  <img src="https://cdn.icon-icons.com/icons2/122/PNG/512/twitter_socialnetwork_20007.png" alt="Twitter" class="w-6 h-6 my-2" />
+                </div>
+                <div>
+                  <img src="https://cdn.icon-icons.com/icons2/805/PNG/512/linkedin_icon-icons.com_65929.png" alt="LinkedIn" class="w-6 h-6 my-2" />
+                </div>
+                <div>
+                  <img src="https://cdn.icon-icons.com/icons2/2972/PNG/512/tiktok_logo_icon_186896.png" alt="TikTok" class="w-6 h-6 my-2" />
+                </div>
+                <div>
+                  <img src="https://cdn.icon-icons.com/icons2/3658/PNG/512/meta_fb_communication_social_media_katana_facebook_icon_228415.png" alt="Facebook" class="w-6 h-6 my-2" />
+                </div>
+              </div>
+            </Link>
+
 
 
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,55 +1,11 @@
-import React from 'react';
-import Ourservices from './Ourservices';
+import React from 'react'
 
 const Home = () => {
   return (
-    <div className="flex flex-wrap">
-      <div className="w-full md:w-1/2 lg:w-1/4 p-4">
-        <div className="bg-gray-200 h-32 flex items-center justify-center">
-          <span>Team member 1</span>
-        </div>
+    <div>
+      home ok
       </div>
+  )
+}
 
-      <div className="w-full md:w-1/2 lg:w-1/4 p-4">
-        <div className="flex flex-wrap">
-          <div className="w-full md:w-1/2">
-            <div className="bg-gray-200 h-32 flex items-center justify-center">
-              <span>Team member 2</span>
-            </div>
-          </div>
-          <div className="w-full md:w-1/2">
-            <div className="bg-gray-200 h-32 flex items-center justify-center">
-              <span>Team member 3</span>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className="w-full lg:w-1/4 p-4">
-        <div className="bg-gray-200 h-32 flex items-center justify-center">
-          <span>Team member 4</span>
-        </div>
-      </div>
-
-      <div className="w-full lg:w-1/4 p-4">
-        <div className="bg-gray-200 h-32 flex items-center justify-center">
-          <span>Team member 5</span>
-        </div>
-      </div>
-
-      <div className="w-full lg:w-1/4 p-4">
-        <div className="bg-gray-200 h-32 flex items-center justify-center">
-          <span>Team member 6</span>
-        </div>
-      </div>
-
-      <div className="w-full lg:w-1/4 p-4">
-        <div className="bg-gray-200 h-32 flex items-center justify-center">
-          <span>Team member 7</span>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default Home;
+export default Home
