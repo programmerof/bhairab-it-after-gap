9796bf606456131b07ed734665892be5c3ccc75f
Your Name <you@example.com>
2023-04-03 16:55:58 +0545

    commit after making all webp and making it work in website

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,69 +1,66 @@
 export default [
-   {
-      title: 'BASE CAMP CAFE',
-      imgUrl: 'assets/BASE CAMP CAFE.webp'
-    },
-    {
-      title: 'basera',
-      imgUrl: 'public/assets/basera.webp'
-    },
-    {
-      title: 'bay leaf',
-      imgUrl: '/assets/bay leaf.jpeg'
-    },
-    {
-      title: 'BHANSA',
-      imgUrl: 'public/assets/BHANSA.webp'
-    },
-    {
-      title: 'CHEF BASANTA',
-      imgUrl: '/assets/CHEF BASANTA.jpeg'
-    },
-    {
-      title: 'EASY RENTALS',
-      imgUrl: '/assets/EASY-RENTALS.jpeg'
-    },
-    {
-      title: 'elrose',
-      imgUrl: '/assets/elrose.jpeg'
-    },
-    {
-      title: 'gham',
-      imgUrl: '/assets/gham.jpeg'
-    },
-    {
-      title: 'HOTEL SALLERI',
-      imgUrl: '/assets/HOTEL-SALLERI.jpeg'
-    },
-    {
-      title: 'loshiva',
-      imgUrl: '/assets/loshiva.jpeg'
-    },
-    {
-      title: 'MAHAJAN',
-      imgUrl: '/assets/MAHAJAN.png'
-    },
-    {
-      title: 'old town',
-      imgUrl: '/assets/old-town.jpeg'
-    },
-    {
-      title: 'THE MANTRA',
-      imgUrl: '/assets/THE-MANTRA.jpeg'
-    },
-    {
-      title: 'tokyo',
-      imgUrl: '/assets/tokyo.jpeg'
-    },
-    {
-      title: 'tripura resort',
-      imgUrl: '/assets/tripura-resort.jpeg'
-    }
-    
-   
-  ].map((portfolio) => {
-    return {
-        ...portfolio,
-        imgUrl: portfolio.imgUrl.replace(/\.[^.]+$/, ".webp")
-    };
-});
+  {
+    title: 'BASE CAMP CAFE',
+    imgUrl: 'assets/BASE CAMP CAFE.webp'
+  },
+  {
+    title: 'basera',
+    imgUrl: '/assets/basera.webp'
+  },
+  {
+    title: 'bay leaf',
+    imgUrl: '/assets/bay leaf.webp'
+  },
+  {
+    title: 'BHANSA',
+    imgUrl: '/assets/BHANSA.webp'
+  },
+  {
+    title: 'CHEF BASANTA',
+    imgUrl: '/assets/CHEF BASANTA.webp'
+  },
+  {
+    title: 'EASY RENTALS',
+    imgUrl: '/assets/easy-rentals.webp'
+  },
+  {
+    title: 'elrose',
+    imgUrl: '/assets/elrose.webp'
+  },
+  {
+    title: 'gham',
+    imgUrl: '/assets/gham.webp'
+  },
+  {
+    title: 'HOTEL SALLERI',
+    imgUrl: '/assets/HOTEL-SALLERI.webp'
+  },
+  {
+    title: 'loshiva',
+    imgUrl: '/assets/loshiva.webp'
+  },
+  {
+    title: 'MAHAJAN',
+    imgUrl: '/assets/MAHAJAN.webp'
+  },
+  {
+    title: 'old town',
+    imgUrl: '/assets/old-town.webp'
+  },
+  {
+    title: 'THE MANTRA',
+    imgUrl: '/assets/THE-MANTRA.webp'
+  },
+  {
+    title: 'tokyo',
+    imgUrl: '/assets/tokyo.webp'
+  },
+  {
+    title: 'tripura resort',
+    imgUrl: '/assets/tripura-resort.webp'
+  }
+
+] 
+
+
+;
