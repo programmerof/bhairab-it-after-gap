063bd7cb6179d38cb4abde4c36e0abba02967d19
Your Name <you@example.com>
2023-04-09 19:52:24 +0545

    const ma no problem

--- src/components/Iit.jsx
+++ src/components/Iit.jsx
@@ -1,33 +1,23 @@
 import React from 'react';
 
 const Iit = () => {
-  const images = [
-    {
-      src: '/people/nitika.webp',
-      alt: 'Image 1',
-    },
-    {
-      src: '/images/image2.jpg',
-      alt: 'Image 2',
-    },
-    {
-      src: '/images/image3.jpg',
-      alt: 'Image 3',
-    },
-    {
-      src: '/images/image4.jpg',
-      alt: 'Image 4',
-    },
-  ];
+    const images = Array.from(Array(12), (_, i) => {
+        const number = (i+1).toString().padStart(2, '0');
+        return {
+          src: `public/iit/IIT (2074)/IIT (2074)-${number}.jpg`,
+          alt: `Image ${i+1}`,
+        };
+      });
+      
 
   return (
     <div className="flex flex-wrap justify-center -mx-4">
       {images.map((image, index) => (
-        <div key={index} className="w-full sm:w-12 md:w-1/3 lg:w-1/4 px-4 py-2">
+        <div key={index} className="w-full lg:w-1/3 px-4 py-2">
           <img
             src={image.src}
             alt={image.alt}
-            className="rounded-lg shadow-md object-cover w-full h-64"
+            className="rounded-lg shadow-md object-cover w-full lg:h-96"
           />
         </div>
       ))}
